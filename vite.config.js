2a1c3a31ba2e4f4674ad6fd59cd9010d05a866e1
edwintantawi <edwintantawi.dev@gmail.com>
2023-12-13 01:17:18 +0700

    feat: setup path alias

--- vite.config.js
+++ vite.config.js
@@ -4,4 +4,12 @@ import { defineConfig } from 'vite';
 // https://vitejs.dev/config/
 export default defineConfig({
   plugins: [react()],
+  resolve: {
+    alias: [
+      {
+        find: '~',
+        replacement: new URL('./src/', import.meta.url).pathname,
+      },
+    ],
+  },
 });
