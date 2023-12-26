7e0816c10be2f42684e1c8041e9111303a295c9c
edwintantawi <edwintantawi.dev@gmail.com>
2023-12-26 15:57:56 +0700

    feat: add active state style

--- src/components/note.jsx
+++ src/components/note.jsx
@@ -1,13 +1,26 @@
 import PropTypes from 'prop-types';
-import { Link } from 'react-router-dom';
+import { Link, useParams } from 'react-router-dom';
 
+import { cn } from '~/utils/classname';
 import { showFormattedDate } from '~/utils/date';
 
 export function Note({ id, title, body, createdAt }) {
+  const params = useParams();
+
+  const isActive = params['note_id'] === id;
+
   return (
-    <article className="relative space-y-2 rounded-md border p-4 hover:bg-muted">
+    <article
+      className={cn('relative space-y-2 rounded-md border p-4 hover:bg-muted', {
+        'bg-primary hover:bg-primary/95': isActive,
+      })}
+    >
       <header className="space-y-1.5">
-        <h3 className="font-semibold">
+        <h3
+          className={cn('font-semibold', {
+            'text-primary-foreground': isActive,
+          })}
+        >
           <Link
             to={id}
             className='after:absolute after:inset-0 after:content-[""]'
@@ -15,11 +28,24 @@ export function Note({ id, title, body, createdAt }) {
             {title}
           </Link>
         </h3>
-        <span className="inline-block rounded-full border bg-slate-200 px-3 py-1 text-xs  text-muted-foreground">
+        <span
+          className={cn(
+            'inline-block rounded-full border bg-slate-200 px-3 py-1 text-xs  text-muted-foreground',
+            {
+              'bg-slate-700 text-primary-foreground': isActive,
+            }
+          )}
+        >
           {showFormattedDate(createdAt)}
         </span>
       </header>
-      <p className="line-clamp-2 text-sm text-muted-foreground">{body}</p>
+      <p
+        className={cn('line-clamp-2 text-sm text-muted-foreground', {
+          'text-primary-foreground': isActive,
+        })}
+      >
+        {body}
+      </p>
     </article>
   );
 }
