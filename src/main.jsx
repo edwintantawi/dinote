05c2687b7bb88fe32ca50ea034b55f72abdfe49a
edwintantawi <edwintantawi.dev@gmail.com>
2024-01-26 22:09:38 +0700

    feat: setup tanstack query

--- src/main.jsx
+++ src/main.jsx
@@ -1,6 +1,8 @@
 import React from 'react';
 import ReactDOM from 'react-dom/client';
 
+import { QueryClient, QueryClientProvider } from '@tanstack/react-query';
+import { ReactQueryDevtools } from '@tanstack/react-query-devtools';
 import { BrowserRouter } from 'react-router-dom';
 
 import { App } from '~/app.jsx';
@@ -9,12 +11,17 @@ import { ThemeProvider } from '~/contexts/theme-context';
 
 import '~/styles/index.css';
 
+const queryClient = new QueryClient();
+
 ReactDOM.createRoot(document.getElementById('root')).render(
   <React.StrictMode>
     <BrowserRouter>
       <ThemeProvider>
         <LocaleProvider>
-          <App />
+          <QueryClientProvider client={queryClient}>
+            <App />
+            <ReactQueryDevtools initialIsOpen={false} />
+          </QueryClientProvider>
         </LocaleProvider>
       </ThemeProvider>
     </BrowserRouter>
